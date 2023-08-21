07ce76d1ef2b926596a05fd07f8c22e5361b6b70
judahpaul16 <judahpaul16@gmail.com>
2023-08-21 02:09:04 -0400

    Style Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React, { useState } from 'react';
 import Keypad from './components/Keypad';
 
 const App: React.FC = () => {
-  const [time, setTime] = useState('');
+  const [pin, setPin] = useState('');
 
   const handleKeyPress = (key: string) => {
-    setTime(time + key);
+    setPin(pin + key);
   };
 
-  const handleClockIn = () => {
+  const handleClockIn = (time: string) => {
     console.log(`Employee clocked in at ${time}`);
-    setTime('');
+    setPin('');
   };
 
   return (
     <div>
-      <h1>Employee Time Clock</h1>
-      <div>Time: {time}</div>
-      <Keypad onKeyPress={handleKeyPress} />
-      <button onClick={handleClockIn}>Clock In</button>
+      <h1>Employee Pin Clock</h1>
+      <div>Pin: {pin}</div>
+      <Keypad onKeyPress={handleKeyPress} onClockIn={handleClockIn}/>
     </div>
   );
 };
--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 
 interface KeypadProps {
   onKeyPress: (key: string) => void;
+  onClockIn: (value: string) => void;
 }
 
-const Keypad: React.FC<KeypadProps> = ({ onKeyPress }) => {
+const Keypad: React.FC<KeypadProps> = ({ onKeyPress, onClockIn }) => {
   return (
-    <div>
+    <div className="keypad-container">
       {['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].map((key) => (
-        <button key={key} onClick={() => onKeyPress(key)}>
+        <button className="keypad-button" key={key} onClick={() => onKeyPress(key)}>
           {key}
         </button>
       ))}
+      <button className="keypad-button"></button>
+      <button className="keypad-button" onClick={() => onClockIn(getTime())}>Clock In</button>
     </div>
   );
 };
+
+function getTime() {
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  return time;
+}
 
 export default Keypad;
