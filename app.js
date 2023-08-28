21c6cd2a61da9fc2499b0545be31579b68c18659
Judah Paul <judahpaul16@gmail.com>
2023-08-28 13:28:51 -0400

    Fixed bcrypt bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const { usersDB, recordsDB } = require('./init-db.js');
 const json2csv = require('json2csv').parse;
